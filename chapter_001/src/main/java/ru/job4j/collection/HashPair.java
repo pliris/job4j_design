8967b5cbef6680f72716f831e2d6b33a89190062
pliris <bonedea733>
2020-09-02 15:18:51 +0300

    8. Реализовать собственную структуру данных - HashMap [#330493]

--- chapter_001/src/main/java/ru/job4j/collection/HashPair.java
+++ chapter_001/src/main/java/ru/job4j/collection/HashPair.java
@@ -1,42 +1,96 @@
 package ru.job4j.collection;
 
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class HashPair<K, V> implements Pair<K, V> {
+public class HashPair<K, V> implements Pair<K, V>, Iterable<K> {
     private NodePair<K, V>[] table;
     private int size;
-    private int loadTable;
+    private double loadTable;
 
     public HashPair() {
         this.table = new NodePair[16];
         this.size = 16;
+        this.loadTable = 0.75;
     }
 
-    static int hash(K key) {
-        int hash = 1;
-        hash = 31 * hash + key.hashCode();
-        return hash;
+    private static int hash(Object key) {
+        int hash;
+        return (key == null) ? 0 : (hash = key.hashCode()) ^ hash >>> 16;
     }
-    private void incrementTable() {
 
+    private int bucketIndex(int hash) {
+        int index;
+        index = (this.size - 1) & hash;
+        return index;
+    }
+
+    private void checkSizeTable(NodePair<K, V>[] table) {
+        if (size + 1 >= size * loadTable) {
+            this.size = size * 2;
+            this.table = Arrays.copyOf(table, size * 2);
+        }
     }
 
     @Override
     public boolean insert(K key, V value) {
-        NodePair<K, V> nodePair
+        NodePair<K, V> nodePair;
+        int hash = hash(key);
+        int index = bucketIndex(hash);
+        checkSizeTable(this.table);
+        if (table[index] == null) {
+            table[index] = new NodePair<>(key, value, null, hash);
+            return true;
+        }
         return false;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (key == null) {
+            return null;
+        }
+        int index = bucketIndex(hash(key));
+        return table[index].value;
     }
 
     @Override
     public boolean delete(K key) {
+        int index = bucketIndex(hash(key));
+        if (table[index].key == key) {
+            table[index] = null;
+            return true;
+        }
         return false;
     }
 
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<K>() {
+            private NodePair<K, V> next;
+            private int cursor = 0;
+            private int size = HashPair.this.size;
+
+            @Override
+            public boolean hasNext() {
+               while (next == null && cursor < size) {
+                   next = table[cursor++];
+               }
+                return next != null;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                K key = next.key;
+                next = next.next;
+                return key;
+            }
+        };
+    }
 
     private static class NodePair<K, V> {
         private NodePair<K, V> next;
@@ -45,26 +99,31 @@ public class HashPair<K, V> implements Pair<K, V> {
         private K key;
         private V value;
 
-        public NodePair(K key, V value, NodePair<K, V> next) {
-            this.hash = this.hashCode();
+        public NodePair(K key, V value, NodePair<K, V> next, int hash) {
+            this.hash = hash;
             this.key = key;
             this.value = value;
             this.next = next;
         }
 
-
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             NodePair<?, ?> nodePair = (NodePair<?, ?>) o;
-            return key.equals(nodePair.key) &&
-                    value.equals(nodePair.value);
+            return key.equals(nodePair.key)
+                    && value.equals(nodePair.value);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(key, value);
+            int hash = 1;
+            hash = 31 * hash + key.hashCode();
+            return hash;
         }
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/collection/HashPairTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class HashPairTest {
+
+    @Test
+    public void insert3IntThenGet1() {
+        HashPair<Integer, String> hashPair = new HashPair<>();
+        hashPair.insert(1,"First");
+        hashPair.insert(2,"Second");
+        hashPair.insert(3,"Third");
+        assertThat(hashPair.get(3), is("Third"));
+    }
+
+    @Test
+    public void insert2IntDelete1ThenTrue() {
+        HashPair<Integer, String> hashPair = new HashPair<>();
+        hashPair.insert(1,"First");
+        hashPair.insert(2,"Second");
+        assertThat(hashPair.delete(2), is(true));
+    }
+
+    @Test
+    public void insert2IntAndInsert1ThenFalse() {
+        HashPair<Integer, String> hashPair = new HashPair<>();
+        hashPair.insert(1,"First");
+        hashPair.insert(2,"Second");
+        assertThat(hashPair.insert(1, "First"), is(false));
+    }
+
+    @Test
+    public void insert3IntThenIterator3IntAndTrueAnd1False() {
+        HashPair<Integer, String> hashPair = new HashPair<>();
+        hashPair.insert(1,"First");
+        hashPair.insert(2,"Second");
+        hashPair.insert(3,"Third");
+        Iterator<Integer> it = hashPair.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(false));
+    }
+
+
+
+
+
+}
