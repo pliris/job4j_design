485ac86a02a2765c15f77c8fa29a2fa9ce70a091
plifis <nikich85@inbox.ru>
2020-09-25 17:54:14 +0400

    Почта. [#184458]

--- chapter_001/src/main/java/ru/job4j/exam/Mail.java
+++ chapter_001/src/main/java/ru/job4j/exam/Mail.java
@@ -165,36 +165,36 @@ public class Mail {
         }
     }
 
-    public static class Email {
-         private String email;
-
-         public Email(String email) {
-             this.email = email;
-         }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Email email1 = (Email) o;
-            return Objects.equals(email, email1.email);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(email);
-        }
-    }
+//    public static class Email {
+//         private String email;
+//
+//         public Email(String email) {
+//             this.email = email;
+//         }
+//
+//        public String getEmail() {
+//            return email;
+//        }
+//
+//        public void setEmail(String email) {
+//            this.email = email;
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) {
+//                return true;
+//            }
+//            if (o == null || getClass() != o.getClass()) {
+//                return false;
+//            }
+//            Email email1 = (Email) o;
+//            return Objects.equals(email, email1.email);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(email);
+//        }
+//    }
 }
