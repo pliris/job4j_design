65fcc5f604f9cc146866ca5128085440e7c448ed
pliris <bonedea733>
2020-09-25 09:37:18 +0300

    mail

--- chapter_001/src/main/java/ru/job4j/exam/Mail.java
+++ chapter_001/src/main/java/ru/job4j/exam/Mail.java
@@ -1,40 +1,65 @@
 package ru.job4j.exam;
 
+import org.junit.validator.PublicClassValidator;
+
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Mail {
     private Map<User, Set<Email>> mapUser = new HashMap<>();
+    private Set<User> set = new HashSet<>();
 
-     public Map<User, Set<Email>> unitMail(Map<User, Set<Email>> accounts) {
-        Set<Email> setEmails = new HashSet<>();
-         for (User user : accounts.keySet()) {
-             Set<Email> emails = accounts.get(user);
-             for (Email tempEmail : emails) {
-                 if (this.findUserByEmail(tempEmail) != null) {
-                     User tempUser = this.findUserByEmail(tempEmail);
-                     setEmails = this.findSetEmails(tempUser);
-                   //  setEmails.add(tempEmail);
-//                     emails = setEmails.stream().
-//                             filter(e -> e.equals(tempEmail)).
-//                             collect(Collectors.toSet());
-                     mapUser.put(user, setEmails.stream().
-                             filter(e -> !e.equals(tempEmail)).
-                             collect(Collectors.toSet()));
-                     mapUser.remove(tempUser, setEmails);
+    public Set<User> process(Set<User> accounts) {
+        Set<User> temp = new HashSet<>();
+        for (User user : accounts) {
+            if (temp.size() == 0) {
+                temp.add(user);
+            }
+            Iterator<User> it = temp.iterator();
+            while (it.hasNext()) {
+                User currentUser = it.next();
+                for (String email : user.getEmails()) {
+                    if (currentUser.containsEmail(email)) {
+                        currentUser.getEmails().addAll(user.getEmails());
+                        break;
+                    }
+                }
+                temp.add(user);
+            }
 
-                 } else if (this.findSetEmails(user) == null) {
-                     Set<Email> tempSet = new HashSet<>();
-                     tempSet.add(tempEmail);
-                     mapUser.put(user, tempSet);
-                 } else {
-                     setEmails = this.findSetEmails(user);
-                     setEmails.add(tempEmail);
-                 }
-             }
-         }
-         return this.mapUser;
-     }
+        }
+        return temp;
+    }
+
+//
+//     public Map<User, Set<Email>> unitMail(Map<User, Set<Email>> accounts) {
+//        Set<Email> setEmails = new HashSet<>();
+//         for (User user : accounts.keySet()) {
+//             Set<Email> emails = accounts.get(user);
+//             for (Email tempEmail : emails) {
+//                 if (this.findUserByEmail(tempEmail) != null) {
+//                     User tempUser = this.findUserByEmail(tempEmail);
+//                     setEmails = this.findSetEmails(tempUser);
+//                   //  setEmails.add(tempEmail);
+////                     emails = setEmails.stream().
+////                             filter(e -> e.equals(tempEmail)).
+////                             collect(Collectors.toSet());
+//                     mapUser.put(user, setEmails.stream().
+//                             filter(e -> !e.equals(tempEmail)).
+//                             collect(Collectors.toSet()));
+//                     mapUser.remove(tempUser, setEmails);
+//
+//                 } else if (this.findSetEmails(user) == null) {
+//                     Set<Email> tempSet = new HashSet<>();
+//                     tempSet.add(tempEmail);
+//                     mapUser.put(user, tempSet);
+//                 } else {
+//                     setEmails = this.findSetEmails(user);
+//                     setEmails.add(tempEmail);
+//                 }
+//             }
+//         }
+//         return this.mapUser;
+//     }
 
      private Set<Email> findSetEmails(User user) {
          return this.mapUser.get(user);
@@ -60,11 +85,21 @@ public class Mail {
 
     public static class User {
          private String name;
+         private HashSet<String> emails;
 
-         public User(String name) {
+
+         public User(String name, HashSet<String> emails) {
              this.name = name;
+             this.emails = emails;
          }
 
+         public Set<String> getEmails() {
+            return this.emails;
+         }
+
+         public boolean containsEmail(String email) {
+             return this.emails.contains(email);
+         }
 
         public String getName() {
             return name;
--- chapter_001/src/test/java/ru/job4j/exam/MailTest.java
+++ chapter_001/src/test/java/ru/job4j/exam/MailTest.java
@@ -3,10 +3,7 @@ package ru.job4j.exam;
 import org.junit.Test;
 import ru.job4j.exam.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -15,39 +12,62 @@ public class MailTest {
 
     @Test
     public void whenThen() {
-        Mail mail = new Mail();
-        Map<Mail.User, Set <Mail.Email>> map = new HashMap<>();
-        map.put(new Mail.User("user1"), Set.of(
-                new Mail.Email("xxx@ya.ru"),
-                new Mail.Email("foo@gmail.com"),
-                new Mail.Email("lol@mail.ru")));
-        map.put(new Mail.User("user2"), Set.of(
-                new Mail.Email("foo@gmail.com"),
-                new Mail.Email("ups@pisem.net")));
-        map.put(new Mail.User("user3"), Set.of(
-                new Mail.Email("xyz@pisem.net"),
-                new Mail.Email("vasya@pupkin.com")));
-        map.put(new Mail.User("user4"), Set.of(
-                new Mail.Email("ups@pisem.net"),
-                new Mail.Email("aaa@bbb.ru")));
-        map.put(new Mail.User("user5"), Set.of(
-                new Mail.Email("xyz@pisem.net")));
+        Set<Mail.User> set = new HashSet<>();
+        set.add(new Mail.User("user1", new HashSet<>(Set.of(("xxx@ya.ru"),                ("foo@gmail.com"),
+                ("lol@mail.ru")))));
+        set.add(new Mail.User("user3", new HashSet<>(Set.of(
+                ("xyz@pisem.net"),
+                ("vasya@pupkin.com")))));
+        set.add(new Mail.User("user5", new HashSet<>(Set.of(
+                ("xyz@pisem.net")))));
+        set = new Mail().process(set);
 
-
-
-        Map<Mail.User, Set<Mail.Email>> exp = new HashMap<>();
-        exp.put(new Mail.User("user1"), Set.of(
-                    new Mail.Email("xxx@ya.ru"),
-                    new Mail.Email("foo@gmail.com"),
-                    new Mail.Email("lol@mail.ru"),
-                    new Mail.Email("ups@pisem.net"),
-                    new Mail.Email("aaa@bbb.ru")));
-        exp.put(new Mail.User("user3"), Set.of(
-                        new Mail.Email("xyz@pisem.net"),
-                        new Mail.Email("vasya@pupkin.com")));
-         Map<Mail.User, Set<Mail.Email>> actual = new HashMap<>(mail.unitMail(map));
-         assertEquals(exp, actual);
+        Set<Mail.User> exp = new HashSet<>();
+        exp.add(new Mail.User("user1", new HashSet<>(Set.of(("xxx@ya.ru"),
+                ("foo@gmail.com"),
+                ("lol@mail.ru")))));
+        exp.add(new Mail.User("user5", new HashSet<>(Set.of(
+                ("vasya@pupkin.com"),
+                ("xyz@pisem.net")))));
+        assertEquals(exp, set);
 
     }
+
+//    @Test
+//    public void whenThen() {
+//        Mail mail = new Mail();
+//        Map<Mail.User, Set <Mail.Email>> map = new HashMap<>();
+//        map.put(new Mail.User("user1"), Set.of(
+//                new Mail.Email("xxx@ya.ru"),
+//                new Mail.Email("foo@gmail.com"),
+//                new Mail.Email("lol@mail.ru")));
+//        map.put(new Mail.User("user2"), Set.of(
+//                new Mail.Email("foo@gmail.com"),
+//                new Mail.Email("ups@pisem.net")));
+//        map.put(new Mail.User("user3"), Set.of(
+//                new Mail.Email("xyz@pisem.net"),
+//                new Mail.Email("vasya@pupkin.com")));
+//        map.put(new Mail.User("user4"), Set.of(
+//                new Mail.Email("ups@pisem.net"),
+//                new Mail.Email("aaa@bbb.ru")));
+//        map.put(new Mail.User("user5"), Set.of(
+//                new Mail.Email("xyz@pisem.net")));
+//
+//
+//
+//        Map<Mail.User, Set<Mail.Email>> exp = new HashMap<>();
+//        exp.put(new Mail.User("user1"), Set.of(
+//                    new Mail.Email("xxx@ya.ru"),
+//                    new Mail.Email("foo@gmail.com"),
+//                    new Mail.Email("lol@mail.ru"),
+//                    new Mail.Email("ups@pisem.net"),
+//                    new Mail.Email("aaa@bbb.ru")));
+//        exp.put(new Mail.User("user3"), Set.of(
+//                        new Mail.Email("xyz@pisem.net"),
+//                        new Mail.Email("vasya@pupkin.com")));
+//         Map<Mail.User, Set<Mail.Email>> actual = new HashMap<>(mail.unitMail(map));
+//         assertEquals(exp, actual);
+//
+//    }
 
 }
