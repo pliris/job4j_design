f85250cae126fef36acc3507ab43b3442355ba44
plifis <nikich85@inbox.ru>
2020-10-06 22:04:08 +0400

    3.0. Тестирование IO [#330504]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,27 +1,38 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
-
+import org.junit.rules.TemporaryFolder;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import java.io.*;
-
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void whenInput2UnavalThen2Period() throws IOException {
-        String source = "C:\\projects\\job4j_design\\source.txt";
-        String target = "C:\\projects\\job4j_design\\target.txt";
+    public void whenTempFolder() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
         Analizy analizy = new Analizy();
-        analizy.unavailable(source, target);
-        FileReader fReader = new FileReader(target);
-        BufferedReader bReader = new BufferedReader(fReader);
-        StringBuilder actual = new StringBuilder();
-        actual.append(bReader.readLine());
-        StringBuilder exp = new StringBuilder();
-        exp.append(" 10:57:01; 10:59:01; 11:01:02; 11:02:02;");
-        assertEquals(exp, actual);
-
-    }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (FileReader reader = new FileReader(target);
+                BufferedReader buffer = new BufferedReader(reader);) {
+           buffer.lines().forEach(rsl::append);
+        }
+        assertThat(rsl, is(" 10:57:01; 10:59:01; 11:01:02; 11:02:02;"));
+        }
 
 }
