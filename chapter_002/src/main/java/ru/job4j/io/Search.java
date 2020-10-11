f04d20496839cadfa529a33b3dfc5cf57e11416c
pliris <bonedea733>
2020-10-11 23:24:36 +0300

    4.1. Сканирование файловой системы. [#330503]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -5,6 +5,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
@@ -13,21 +14,22 @@ public class Search {
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
-        SearchFiles searcher = new SearchFiles(ext);
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
     static class SearchFiles extends SimpleFileVisitor<Path> {
         private List<Path> pathsList = new ArrayList<>();
-        private String str;
-        SearchFiles(String ext) {
-                   this.str = ext;
+        Predicate<Path> filter;
+        SearchFiles(Predicate<Path> filter) {
+                   this.filter = filter;
         }
+
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            if (file.getFileName().toString().endsWith(str)) {
-               pathsList.add(file);
+            if (filter.test(file)) {
+                pathsList.add(file);
             }
             return super.visitFile(file, attrs);
         }
