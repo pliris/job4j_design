dea221117055a0edd9a2ed9f4f17affebaa01910
plifis <nikich85@inbox.ru>
2020-11-16 11:12:21 +0400

    3. Slf4j - вывод переменных. [#330500]

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -12,5 +12,17 @@ public class UsageLog4j {
             LOG.info("info message");
             LOG.warn("warn message");
             LOG.error("error message");
+
+            String name = "Nikita";
+            int age = 99;
+            byte weight = 70;
+            short exp = 0;
+            long salary = 1000000;
+            float unKnow = 1.1f;
+            double height = 1.72;
+            char sex = 'm';
+            boolean student = true;
+            LOG.debug("user info name : {}, age : {}, weight : {}, exp : {}, salary : {}, unKnow : {}, height : {}, sex : {}, student : {}  ",
+                    name, age, weight, exp, salary, unKnow, height, sex, student);
         }
 }
