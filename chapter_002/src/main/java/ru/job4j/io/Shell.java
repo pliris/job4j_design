11c92a7442310ac9324b28f76e3fbdea2e9597c6
plifis <nikich85@inbox.ru>
2020-11-30 18:49:18 +0300

    Поиск файлов по критерию

--- chapter_002/src/main/java/ru/job4j/io/Shell.java
+++ chapter_002/src/main/java/ru/job4j/io/Shell.java
@@ -3,14 +3,14 @@ package ru.job4j.io;
 import java.nio.file.Path;
 
 public class Shell {
-    private String tree = "/user/local";
+    private final String tree = "/user/local/dir1/dir2";
     private String current = "/";
 
     public void cd(String path) {
     if (path.contains("..") || path.contains("/")) {
         int i = Path.of(current).getNameCount();
         if (i > 1) {
-            this.current = current.substring(0, i - 1);
+            this.current = current.substring(0, this.current.lastIndexOf("/"));
         } else {
             this.current = "/";
         }
@@ -24,5 +24,16 @@ public class Shell {
     }
     public String pwd() {
         return current;
+    }
+
+    public static void main(String[] args) {
+        Shell shell = new Shell();
+        shell.cd("dir1");
+        System.out.println(shell.pwd());
+
+        shell.cd("dir2");
+        System.out.println(shell.pwd());
+        shell.cd("..");
+        System.out.println(shell.pwd());
     }
 }
