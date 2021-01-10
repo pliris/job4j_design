023a9de0cf8c9ee2725d98d8906ba4671568b495
plifis <nikich85@inbox.ru>
2021-01-10 21:24:49 +0300

    SQL [#224183]

---
+++ chapter_003/src/main/java/ru/job4j/sql/exam/exam.sql
@@ -0,0 +1,58 @@
+CREATE TABLE meeting(
+                        id serial primary key,
+                        name varchar(50)
+);
+
+CREATE TABLE users(
+                      id serial primary key,
+                      name varchar(50)
+);
+
+CREATE TABLE status(
+                       id serial primary key,
+                       status boolean,
+                       meeting_id int references meeting (id),
+                       user_id int references users (id)
+);
+
+
+INSERT INTO meeting(name)
+values('jpoint'),
+      ('joker'),
+      ('hackdays'),
+      ('dom 2');
+INSERT INTO users(name)
+values('Nikita'),
+      ('Alexey'),
+      ('Bill'),
+      ('Tina'),
+      ('Linda'),
+      ('Pete');
+
+INSERT INTO status(status, meeting_id, user_id)
+values(true, '1', '1'),
+      (true, '2', '1'),
+      (false, '3', '1'),
+      (false, '4', '1'),
+      (true, '1', '2'),
+      (false, '2', '2'),
+      (false, '3', '2'),
+      (false, '4', '2');
+
+select status.status, meeting.name, COUNT(*) as total
+from status
+         INNER JOIN meeting
+                    ON status.meeting_id = meeting.id
+         JOIN users
+              ON status.user_id = users.id
+GROUP BY  status.status, meeting.name
+HAVING status = true;
+
+select meeting.name, COUNT (*)
+from status
+         LEFT OUTER JOIN meeting
+                         ON status.meeting_id = meeting.id
+WHERE status.status = false
+GROUP BY meeting.name
+HAVING COUNT(*) = 4
+;
