3f8a53bad8f2d0cb3bfe9bd20803d56efbd0437e
plifis <nikich85@inbox.ru>
2021-03-09 16:17:27 +0300

    Поиск файлов по критерию [#330525]

--- chapter_002/src/main/java/ru/job4j/io/filefinder/ArgsSearch.java
+++ chapter_002/src/main/java/ru/job4j/io/filefinder/ArgsSearch.java
@@ -1,4 +1,4 @@
-package ru.job4j.io;
+package ru.job4j.io.filefinder;
 
 
 public class ArgsSearch {
--- chapter_002/src/main/java/ru/job4j/io/filefinder/SearchFile.java
+++ chapter_002/src/main/java/ru/job4j/io/filefinder/SearchFile.java
@@ -1,15 +1,9 @@
-package ru.job4j.io;
+package ru.job4j.io.filefinder;
 
 import java.io.*;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 public class SearchFile {
     private ArgsSearch argsSearch;
